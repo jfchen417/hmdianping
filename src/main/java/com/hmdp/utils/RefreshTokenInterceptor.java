608f44bc735d02e91931afd5d5262fb373f98b8a
jfchen417 <jiangfan_chen@outlook.com>
2025-05-19 03:34:50 +0800

    Token刷新，登录拦截器的优化。搭建拦截器链，使用一级拦截器完成用户的登录校验，并使用二级拦截器解决Token刷新的问题，实现了只要用户在访问界面，就能一直刷新Token的功能。

---
+++ src/main/java/com/hmdp/utils/RefreshTokenInterceptor.java
@@ -0,0 +1,52 @@
+package com.hmdp.utils;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
+import com.hmdp.dto.UserDTO;
+import com.hmdp.entity.User;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class RefreshTokenInterceptor implements HandlerInterceptor {
+
+    private StringRedisTemplate stringRedisTemplate;
+
+    // 只能使用构造函数注入，不能用@Autowired等注解，因为是手动创建的对象，没有自动依赖注入
+    public RefreshTokenInterceptor(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        // 1. 获取请求头中的Token
+        String token = request.getHeader("authorization");
+        if (StrUtil.isBlank(token)) {
+            return true;
+        }
+        // 2. 基于Token 获取 Redis 中的用户
+        String key = RedisConstants.LOGIN_USER_KEY + token;
+        Map<Object, Object> userMap = stringRedisTemplate.opsForHash()
+                .entries(key);
+
+        // 3. 判断用户是否存在
+        if (userMap.isEmpty()) {
+            return true;
+        }
+
+        // 5. 将查询到的Hash数据转化为UserDTO对象
+        UserDTO userDTO = BeanUtil.fillBeanWithMap(userMap, new UserDTO(), false);
+        // 6. 存在，保存用户信息到ThreadLocal
+        UserHolder.saveUser(userDTO);
+
+        // 7. 刷新Token 有效期
+        stringRedisTemplate.expire(key, RedisConstants.LOGIN_USER_TTL, TimeUnit.MINUTES);
+        // 8. 放行
+        return true;
+    }
+}
